cc45e6545102d0610fb3b2f276ad91273a0af0f3
Rohan Tangri <rohan.tangri@gmail.com>
2021-09-10 17:10:32 +0100

    add Actor

--- anvil/models/__init__.py
+++ anvil/models/__init__.py
@@ -1,5 +1,4 @@
+from anvil.models.actors import Actor
 from anvil.models.critics import Critic
 
-__all__ = [
-    "Critic",
-]
+__all__ = ["Critic", "Actor"]
---
+++ anvil/models/actors.py
@@ -0,0 +1,35 @@
+from typing import List, Union
+
+import torch as T
+
+from anvil.models.utils import get_device
+
+
+class Actor(T.nn.Module):
+    """
+    The actor network which approximates the Q or Value functions. Define the heads as a
+    list to allow for potential multiple head output networks.
+
+    :param encoder: the encoder network
+    :param torso: the torso network
+    :param heads: a list of network heads
+    """
+
+    def __init__(
+        self,
+        encoder: T.nn.Module,
+        torso: T.nn.Module,
+        heads: List[T.nn.Module],
+        device: Union[T.device, str] = "auto",
+    ):
+        super().__init__()
+        device = get_device(device)
+        self.encoder = encoder.to(device)
+        self.torso = torso.to(device)
+        self.heads = [head.to(device) for head in heads]
+
+    def forward(self, input: T.Tensor) -> List[T.Tensor]:
+        out = self.encoder(input)
+        out = self.torso(out)
+        out = [head(out) for head in self.heads]
+        return out
