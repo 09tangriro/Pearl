df9c714f24127f813742f4c9dc3725e837b1cc4e
Rohan Tangri <rohan.tangri@gmail.com>
2021-09-17 18:25:19 +0100

    add docs

--- anvil/updaters/utils.py
+++ anvil/updaters/utils.py
@@ -4,6 +4,14 @@ import torch as T
 def sample_forward_kl_divergence(
     target_dist_prob: T.Tensor, sample_dist_prob: T.Tensor
 ) -> T.Tensor:
+    """
+    Sample forward KL Divergence element-wise
+    https://towardsdatascience.com/approximating-kl-divergence-4151c8c85ddd
+
+    :param target_dist_prob: probabilities from target distribution, p(x)
+    :param sample_dist_prob: probabilites from sample distribution, q(x)
+    :return: element-wise kl approximation
+    """
     ratio = target_dist_prob / sample_dist_prob
     return ratio * T.log(ratio) - (ratio - 1)
 
@@ -11,5 +19,13 @@ def sample_forward_kl_divergence(
 def sample_reverse_kl_divergence(
     target_dist_prob: T.Tensor, sample_dist_prob: T.Tensor
 ) -> T.Tensor:
+    """
+    Sample reverse KL Divergence element-wise
+    https://towardsdatascience.com/approximating-kl-divergence-4151c8c85ddd
+
+    :param target_dist_prob: probabilities from target distribution, p(x)
+    :param sample_dist_prob: probabilites from sample distribution, q(x)
+    :return: element-wise kl approximation
+    """
     ratio = target_dist_prob / sample_dist_prob
     return (ratio - 1) - T.log(ratio)
