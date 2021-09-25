9236f0ed1ef18b58592b388e87c6daf11bd801f4
Rohan Tangri <rohan.tangri@gmail.com>
2021-09-25 16:35:55 +0100

    add gae

--- anvil/signal_processing/sample_estimators.py
+++ anvil/signal_processing/sample_estimators.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 from numba import jit
 
@@ -43,3 +45,36 @@ def TD_zero(
     :param gamma: the discount factor of future rewards
     """
     return rewards + ((1 - dones) * gamma * next_values)
+
+
+@jit(nopython=True, parallel=True)
+def generalized_advantage_estimate(
+    rewards: np.ndarray,
+    old_values: np.ndarray,
+    new_values: np.ndarray,
+    dones: np.ndarray,
+    gamma: float = 0.99,
+    gae_lambda: float = 0.95,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generalized advantage estimate of a trajectory: https://towardsdatascience.com/generalized-advantage-estimate-maths-and-code-b5d5bd3ce737
+
+    :param old_values: value function result with old_state input
+    :param new_values: value function result with new_state input
+    :param rewards: agent reward of taking actions in the environment
+    :param dones: flag for end of episode
+    :param gamma: exponential mean discount
+    :param gae_lambda: trajectory discount
+    """
+    dones = 1 - dones
+    batch_size = rewards.shape[0]
+
+    advantage = np.zeros(batch_size + 1)
+
+    for t in np.flip(np.arange(batch_size)):
+        delta = rewards[t] + (gamma * new_values[t] * dones[t]) - old_values[t]
+        advantage[t] = delta + (gamma * gae_lambda * advantage[t + 1] * dones[t])
+
+    value_target = advantage[:batch_size] + old_values
+
+    return advantage[:batch_size], value_target
--- tests/test_signal_processing.py
+++ tests/test_signal_processing.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-from anvil.signal_processing.sample_estimators import TD_lambda, TD_zero
+from anvil.signal_processing.sample_estimators import (
+    TD_lambda,
+    TD_zero,
+    generalized_advantage_estimate,
+)
 
 
 def test_TD_lambda():
@@ -15,12 +19,30 @@ def test_TD_lambda():
 
 
 def test_TD_zero():
-    rewards = np.ones(shape=(2, 3))
-    next_values = np.ones(shape=(2, 3))
-    dones = np.zeros(shape=(2, 3))
+    rewards = np.ones(shape=(3,))
+    next_values = np.ones(shape=(3,))
+    dones = np.zeros(shape=(3,))
 
     actual_returns = TD_zero(rewards, next_values, dones, gamma=1)
-    expected_returns = np.array([[2, 2, 2], [2, 2, 2]], dtype=np.float32)
+    expected_returns = np.array([2, 2, 2], dtype=np.float32)
     print(actual_returns)
 
+    np.equal(actual_returns, expected_returns)
+
+
+def test_generalized_advantage_estimate():
+    rewards = np.ones(shape=(3,))
+    old_values = np.ones(shape=(3,))
+    new_values = np.ones(shape=(3,))
+    dones = np.zeros(shape=(3,))
+
+    actual_advantages, actual_returns = generalized_advantage_estimate(
+        rewards, old_values, new_values, dones, gamma=1, gae_lambda=1
+    )
+    expected_advantages, expected_returns = (
+        np.array([3, 2, 1], dtype=np.float32),
+        np.array([4, 3, 2], dtype=np.float32),
+    )
+
+    np.equal(actual_advantages, expected_advantages)
     np.equal(actual_returns, expected_returns)
