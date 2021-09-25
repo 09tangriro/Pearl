07b25250549d234c2e98d0f8681d8cd3b0c50ef1
Rohan Tangri <rohan.tangri@gmail.com>
2021-09-25 18:55:04 +0100

    move doc string

--- anvil/signal_processing/normalizers.py
+++ anvil/signal_processing/normalizers.py
@@ -5,10 +5,10 @@ from numba import jit
 
 @jit(nopython=True, parallel=True)
 def _even_normalizer(data: np.ndarray, high: np.ndarray, low: np.ndarray) -> np.ndarray:
-    """Normalize data in the range [-1, 1]"""
     if np.all(np.abs(low) == np.abs(high)):
         return data / high
 
 
 def even_normalizer(data: np.ndarray, space: Space):
+    """Normalize data in the range [-1, 1]"""
     return _even_normalizer(data, space.high, space.low)
