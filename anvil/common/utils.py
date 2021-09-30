0bad3e49595d2cf16b41642a3fcbc06b80f494ba
Rohan Tangri <rohan.tangri@gmail.com>
2021-09-30 14:06:09 +0100

    move buffer utils to common utils

--- anvil/common/utils.py
+++ anvil/common/utils.py
@@ -1,7 +1,8 @@
-from typing import Tuple, Union
+from typing import Dict, Tuple, Union
 
 import numpy as np
 import torch as T
+from gym import spaces
 
 
 def get_device(device: Union[T.device, str]) -> T.device:
@@ -59,3 +60,31 @@ def torch_to_numpy(*data) -> Tuple[np.ndarray]:
             )
 
     return tuple(result)
+
+
+def get_space_shape(
+    space: spaces.Space,
+) -> Union[Tuple[int, ...], Dict[str, Tuple[int, ...]]]:
+    """
+    Get the shape of a space (useful for the buffers).
+    :param space:
+    :return:
+    """
+    if isinstance(space, spaces.Box):
+        return space.shape
+    elif isinstance(space, spaces.Discrete):
+        # an int
+        return (1,)
+    elif isinstance(space, spaces.MultiDiscrete):
+        # Number of discrete features
+        return (int(len(space.nvec)),)
+    elif isinstance(space, spaces.MultiBinary):
+        # Number of binary features
+        return (int(space.n),)
+    elif isinstance(space, spaces.Dict):
+        return {
+            key: get_space_shape(subspace) for (key, subspace) in space.spaces.items()
+        }
+
+    else:
+        raise NotImplementedError(f"{space} observation space is not supported")
