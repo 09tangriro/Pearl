a1bfa79a5ec591284170ae11f2236c5ad83a119c
Rohan Tangri <rohan.tangri@gmail.com>
2021-10-06 09:36:47 +0100

    pipx->pip3

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 echo ---------------------
-echo Installing pipx and ensuring path
-echo ---------------------
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-sudo apt install python3.8-venv
-
-echo ---------------------
 echo Installing and creating poetry virtual environment
 echo ---------------------
-pipx install poetry
+pip3 install poetry
 poetry install
 echo ""
 
 echo ---------------------
 echo Installing pre-commit hooks
 echo ---------------------
-pipx install pre-commit
+pip3 install pre-commit
 pre-commit install
 echo ""
 
