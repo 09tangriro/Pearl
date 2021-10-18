58d22fb57a571cfbebb54d7bd8d574dddf39add8
Rohan Tangri <rohan.tangri@gmail.com>
2021-10-18 11:24:42 +0100

    remove echos

--- scripts/setup_dev.bat
+++ scripts/setup_dev.bat
@@ -1,17 +1,9 @@
-echo ---------------------
 echo Installing and creating poetry virtual environment
-echo ---------------------
 pip install poetry
 poetry install
-echo ""
 
-echo ---------------------
 echo Installing pre-commit hooks
-echo ---------------------
 pip install pre-commit
 pre-commit install
-echo ""
 
-echo ---------------------
 echo DONE
-echo ---------------------
