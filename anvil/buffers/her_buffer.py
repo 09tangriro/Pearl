209d5ec9126bbba90c20b01df5a81050fd82854f
Rohan Tangri <rohan.tangri@gmail.com>
2021-10-25 20:19:12 +0100

    her v0

---
+++ anvil/buffers/her_buffer.py
@@ -0,0 +1,210 @@
+from typing import Dict, Union
+
+import numpy as np
+import torch as T
+from gym.core import GoalEnv
+from gym.spaces.space import Space
+
+from anvil.buffers.base_buffer import BaseBuffer
+from anvil.common.enumerations import GoalSelectionStrategy, TrajectoryType
+from anvil.common.type_aliases import Trajectories
+
+
+class HERBuffer(BaseBuffer):
+    """Hindsight Exprience Replay (HER) Buffer"""
+
+    def __init__(
+        self,
+        env: GoalEnv,
+        buffer_size: int,
+        observation_space: Space,
+        action_space: Space,
+        n_envs: int = 1,
+        goal_selection_strategy: Union[str, GoalSelectionStrategy] = "future",
+        n_sampled_goal: int = 4,
+        device: Union[str, T.device] = "auto",
+    ) -> None:
+        super().__init__(
+            buffer_size, observation_space, action_space, n_envs=n_envs, device=device
+        )
+        self.env = env
+        self.next_observations = np.zeros(
+            (self.buffer_size, self.n_envs) + self.obs_shape,
+            dtype=observation_space.dtype,
+        )
+        self.desired_goals = np.zeros(
+            (self.buffer_size, self.n_envs) + self.obs_shape,
+            dtype=observation_space.dtype,
+        )
+        self.achieved_goals = np.zeros(
+            (self.buffer_size, self.n_envs) + self.obs_shape,
+            dtype=observation_space.dtype,
+        )
+        self.next_achieved_goals = np.zeros(
+            (self.buffer_size, self.n_envs) + self.obs_shape,
+            dtype=observation_space.dtype,
+        )
+        self.her_rewards = np.zeros(
+            (self.buffer_size, self.n_envs, 1), dtype=np.float32
+        )
+
+        # Keep track of where in the data structure episodes end
+        # Declare as np array to sacrifice memory for performance
+        self.episode_end_indices = np.zeros((self.buffer_size, self.n_envs))
+        self.index_episode_map = np.zeros((self.buffer_size, self.n_envs))
+        self.episode = 0
+
+        self._check_system_memory(
+            self.observations,
+            self.actions,
+            self.rewards,
+            self.dones,
+            self.next_observations,
+            self.desired_goals,
+            self.achieved_goals,
+            self.next_achieved_goals,
+            self.her_rewards,
+            self.episode_end_indices,
+            self.index_episode_map,
+        )
+
+        if isinstance(goal_selection_strategy, str):
+            self.goal_section_strategy = GoalSelectionStrategy(
+                goal_selection_strategy.lower()
+            )
+        else:
+            self.goal_section_strategy = goal_selection_strategy
+
+        self.her_ratio = 1 - (1.0 / (n_sampled_goal + 1))
+
+    def _select_goal(self) -> np.ndarray:
+        if self.goal_section_strategy == GoalSelectionStrategy.FINAL:
+            return self.next_observations[self.pos]
+
+    def add_trajectory(
+        self,
+        observation: Dict[str, np.ndarray],
+        action: Union[np.ndarray, int],
+        reward: float,
+        next_observation: Dict[str, np.ndarray],
+        done: bool,
+    ) -> None:
+        self.observations[self.pos] = observation["observation"]
+        self.achieved_goals[self.pos] = observation["achieved_goal"]
+        self.desired_goals[self.pos] = observation["desired_goal"]
+        self.rewards[self.pos] = reward
+        self.actions[self.pos] = action
+        self.next_observations[self.pos] = next_observation["observation"]
+        self.next_achieved_goals[self.pos] = next_observation["achieved_goal"]
+        self.dones[self.pos] = done
+        self.index_episode_map[self.pos] = self.episode
+
+        if done:
+            self.episode_end_indices[self.episode] = self.pos
+            self.episode += 1
+
+            if self.episode == self.buffer_size:
+                self.episode = 0
+
+        self.pos += 1
+        if self.pos == self.buffer_size:
+            self.full = True
+            self.pos = 0
+
+    def _sample_goals(self, her_batch_inds: np.ndarray) -> np.ndarray:
+        her_episodes = self.index_episode_map[her_batch_inds]
+        episode_end_indices = self.episode_end_indices[her_episodes]
+
+        # Goal is the last state in the episode
+        if self.goal_section_strategy == GoalSelectionStrategy.FINAL:
+            goal_indices = self.next_observations[episode_end_indices]
+
+        # Goal is a random state in the same episode observed after current transition
+        elif self.goal_section_strategy == GoalSelectionStrategy.FUTURE:
+            goal_indices = np.random.randint(her_batch_inds, episode_end_indices + 1)
+
+        else:
+            raise ValueError(
+                f"Strategy {self.goal_section_strategy} for samping goals not supported."
+            )
+
+        return self.achieved_goals[goal_indices]
+
+    def sample(
+        self, batch_size: int, dtype: Union[str, TrajectoryType] = "numpy"
+    ) -> Trajectories:
+        if isinstance(dtype, str):
+            dtype = TrajectoryType(dtype.lower())
+        her_batch_size = int(batch_size * self.her_ratio)
+
+        # Sample batch indices
+        if self.full:
+            batch_inds = (
+                np.random.randint(1, self.buffer_size, size=batch_size) + self.pos
+            ) % self.buffer_size
+        else:
+            batch_inds = np.random.randint(0, self.pos, size=batch_size)
+        # Reserve HER goal indices
+        her_batch_inds = batch_inds[:her_batch_size]
+        replay_inds = batch_inds[her_batch_size:]
+
+        her_goals = self._sample_goals(her_batch_inds)
+        # the new state depends on the previous state and action
+        # s_{t+1} = f(s_t, a_t)
+        # so the next_achieved_goal depends also on the previous state and action
+        # because we are in a GoalEnv:
+        # r_t = reward(s_t, a_t) = reward(next_achieved_goal, desired_goal)
+        # therefore we have to use "next_achieved_goal" and not "achieved_goal"
+        her_rewards = self.env.compute_reward(
+            self.next_achieved_goals[her_batch_inds], her_goals, {}
+        )
+
+        desired_goals = np.concatenate([her_goals, self.desired_goals[replay_inds]])
+        rewards = np.concatenate([her_rewards, self.rewards[batch_inds[replay_inds]]])
+        observations = np.concatenate([self.observations, desired_goals], axis=2)
+        actions = self.actions[batch_inds]
+        next_observations = np.concatenate(
+            [self.next_observations, desired_goals], axis=2
+        )
+        dones = self.dones[batch_inds]
+
+        return Trajectories(
+            observations=observations,
+            actions=actions,
+            rewards=rewards,
+            next_observations=next_observations,
+            dones=dones,
+        )
+
+    def last(self, batch_size: int, dtype: Union[str, TrajectoryType] = "numpy"):
+        if isinstance(dtype, str):
+            dtype = TrajectoryType(dtype.lower())
+        assert batch_size <= self.buffer_size
+
+        start_idx = self.pos - batch_size
+        if start_idx < 0:
+            batch_inds = np.concatenate((np.arange(start_idx, 0), np.arange(self.pos)))
+        else:
+            batch_inds = np.arange(start_idx, self.pos)
+
+        observations = self.observations[batch_inds]
+        actions = self.actions[batch_inds]
+        rewards = self.rewards[batch_inds]
+        next_observations = self.next_observations[batch_inds]
+        dones = self.dones[batch_inds]
+
+        # return torch tensors instead of numpy arrays
+        if dtype == TrajectoryType.TORCH:
+            observations = T.tensor(observations).to(self.device)
+            actions = T.tensor(actions).to(self.device)
+            rewards = T.tensor(rewards).to(self.device)
+            next_observations = T.tensor(next_observations).to(self.device)
+            dones = T.tensor(dones).to(self.device)
+
+        return Trajectories(
+            observations=observations,
+            actions=actions,
+            rewards=rewards,
+            next_observations=next_observations,
+            dones=dones,
+        )
