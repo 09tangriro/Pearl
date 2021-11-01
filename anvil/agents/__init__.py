29896b93b1be596bfa0f66fad376184fa50bd187
Rohan Tangri <rohan.tangri@gmail.com>
2021-11-01 18:21:37 +0000

    add agents to init

--- anvil/agents/__init__.py
+++ anvil/agents/__init__.py
@@ -1,3 +1,5 @@
 from anvil.agents.base_agent import BaseAgent
+from anvil.agents.ddpg import DDPG
+from anvil.agents.dqn import DQN
 
-__all__ = ["BaseAgent"]
+__all__ = ["BaseAgent", "DDPG", "DQN"]
