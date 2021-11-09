c7d46c47e0122381a35dd3f64cb76e865527fee1
Rohan Tangri <rohan.tangri@gmail.com>
2021-11-09 14:59:26 +0000

    add placeholder base agent test

---
+++ tests/test_base_agents.py
@@ -0,0 +1,57 @@
+import gym
+import numpy as np
+import torch as T
+
+from anvilrl.agents.base_agents import BaseDeepAgent
+from anvilrl.buffers import ReplayBuffer
+from anvilrl.models.actor_critics import Actor, ActorCritic, Critic
+from anvilrl.models.encoders import IdentityEncoder
+from anvilrl.models.heads import ContinuousQHead
+from anvilrl.models.torsos import MLP
+from anvilrl.settings import ExplorerSettings
+
+
+class MockDeepAgent(BaseDeepAgent):
+    def _fit():
+        pass
+
+
+env = gym.make("Pendulum-v0")
+envs = gym.vector.make("Pendulum-v0", num_envs=2, asynchronous=False)
+
+encoder = IdentityEncoder()
+torso = MLP(layer_sizes=[3, 64, 32], activation_fn=T.nn.ReLU)
+head = ContinuousQHead(input_shape=32)
+model = ActorCritic(
+    actor=Actor(encoder, torso, head), critic=Critic(encoder, torso, head)
+)
+
+agent = MockDeepAgent(
+    env=env,
+    model=model,
+    buffer_class=ReplayBuffer,
+    explorer_settings=ExplorerSettings(start_steps=0),
+)
+vec_agent = MockDeepAgent(
+    env=envs,
+    model=model,
+    buffer_class=ReplayBuffer,
+    explorer_settings=ExplorerSettings(start_steps=0),
+)
+
+
+def test_step_env():
+    env.seed(0)
+    observation = env.reset()
+    action = model(observation).detach().numpy()
+    expected_next_obs, _, _, _ = env.step(action)
+    actual_next_obs = agent.step_env(observation)
+
+    assert isinstance(actual_next_obs, np.ndarray)
+
+    observation = envs.reset()
+    action = model(observation).detach().numpy()
+    expected_next_obs, _, _, _ = env.step(action)
+    actual_next_obs = vec_agent.step_env(observation)
+
+    assert isinstance(actual_next_obs, np.ndarray)
