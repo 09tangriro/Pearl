c349495e82579f2a0db0935a18b81363345e3619
Rohan Tangri <rohan.tangri@gmail.com>
2021-11-18 19:05:32 +0000

    configure __init__ files

--- anvilrl/agents/__init__.py
+++ anvilrl/agents/__init__.py
@@ -1,5 +1,6 @@
 from anvilrl.agents.base_agents import BaseDeepAgent
 from anvilrl.agents.ddpg import DDPG
 from anvilrl.agents.dqn import DQN
+from anvilrl.agents.es import ES
 
-__all__ = ["BaseDeepAgent", "DDPG", "DQN"]
+__all__ = ["BaseDeepAgent", "DDPG", "DQN", "ES"]
