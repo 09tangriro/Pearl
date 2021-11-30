9634703e5d92fc4d2a7f632c38ae6fe3b4889492
Rohan Tangri <rohan.tangri@gmail.com>
2021-11-30 14:46:47 +0000

    add GA to init

--- anvilrl/agents/__init__.py
+++ anvilrl/agents/__init__.py
@@ -2,5 +2,6 @@ from anvilrl.agents.base_agents import BaseDeepAgent
 from anvilrl.agents.ddpg import DDPG
 from anvilrl.agents.dqn import DQN
 from anvilrl.agents.es import ES
+from anvilrl.agents.ga import GA
 
-__all__ = ["BaseDeepAgent", "DDPG", "DQN", "ES"]
+__all__ = ["BaseDeepAgent", "DDPG", "DQN", "ES", "GA"]
