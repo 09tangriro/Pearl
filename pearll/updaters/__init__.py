2fee9311b4c85d278419f7945113020d66a42338
Rohan Tangri <rohan.tangri@gmail.com>
2022-03-22 18:42:05 +0000

    update __init__

--- pearll/updaters/__init__.py
+++ pearll/updaters/__init__.py
@@ -1,5 +1,11 @@
 from pearll.updaters.actors import BaseActorUpdater
 from pearll.updaters.critics import BaseCriticUpdater
+from pearll.updaters.environment import BaseDeepUpdater
 from pearll.updaters.evolution import BaseEvolutionUpdater
 
-__all__ = ["BaseActorUpdater", "BaseCriticUpdater", "BaseEvolutionUpdater"]
+__all__ = [
+    "BaseActorUpdater",
+    "BaseCriticUpdater",
+    "BaseEvolutionUpdater",
+    "BaseDeepUpdater",
+]
