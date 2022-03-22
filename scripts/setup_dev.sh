de78b1dd5ea13147ce08d2fc5756966804ca3168
Rohan Tangri <rohan.tangri@gmail.com>
2022-03-22 11:01:31 +0000

    install pre commit in poetry env

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -9,10 +9,25 @@ echo ""
 echo ---------------------
 echo Installing pre-commit hooks
 echo ---------------------
-pip3 install pre-commit
-pre-commit install
+poetry run pre-commit install
 echo ""
 
 echo ---------------------
 echo DONE
 echo ---------------------
+
+echo ""
+echo =================================================================================
+echo ""
+
+echo A python virtual environment has been created at:
+poetry config --local virtualenvs.path
+
+echo ""
+echo To run unit tests locally, run:
+echo ""
+echo    poetry run pytest OR poetry run scripts/run_tests.sh
+
+echo ""
+echo =================================================================================
+echo ""
--- scripts/windows_setup_dev.bat
+++ scripts/windows_setup_dev.bat
@@ -3,7 +3,22 @@ pip install poetry
 poetry install
 
 echo Installing pre-commit hooks
-pip install pre-commit
-pre-commit install
+poetry run pre-commit install
 
 echo DONE
+
+echo.
+echo =================================================================================
+echo.
+
+echo A python virtual environment has been created at:
+poetry config --local virtualenvs.path
+
+echo.
+echo To run unit tests locally, run:
+echo.
+echo    poetry run pytest OR poetry run scripts/run_tests.sh
+
+echo .
+echo =================================================================================
+echo.
